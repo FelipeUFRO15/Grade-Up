b2772b8f87deaa190fbf14dab1f0514342342ec6
FelipeUFRO15 <f.acuna08@ufromail.cl>
2015-09-30 11:51:52 -0200

    Primera versión

--- GItHub1/src/Calculadora.java
+++ GItHub1/src/Calculadora.java
@@ -5,7 +5,7 @@ public class Calculadora {
 		int menu1 = 0;
 		int num1=0, num2=0;
 		do{
-			System.out.println("\n1.- Sumar dos númerosHUTOYJIHOIFI");
+			System.out.println("\n1.- Sumar dos números");
 			System.out.println("2.- Restar dos números");
 			System.out.println("3.- Multiplicar dos números");
 			System.out.println("4.- Dividir dos números");

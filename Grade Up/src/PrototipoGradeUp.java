7f8a126a07ff09512394e2ab56c50647b9da2cb5
FelipeUFRO15 <f.acuna08@ufromail.cl>
2015-10-21 22:34:10 -0300

    Change-Id: I10ac3a4b1d38fb4fad3b24ccc4ad551b6a3d98df

---
+++ Grade Up/src/PrototipoGradeUp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class PrototipoGradeUp {
+
+	public static double leerNúmeros() {
+		double nota = 0.0;
+		Scanner scn = new Scanner(System.in);
+		
+		return nota;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
